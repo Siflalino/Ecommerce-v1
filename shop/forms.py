be7988107abc2b48dd0e16d43ede850719fd54c8
Siflalino <167150806+Siflalino@users.noreply.github.com>
2026-02-13 16:59:45 -0500

    Update forms.py

--- shop/forms.py
+++ shop/forms.py
@@ -18,37 +18,3 @@ class CommentaireForm(forms.ModelForm):
     class Meta:
         model = Commentaire
         fields = ['note', 'commentaire']
-
-#     def save(self, commit=True):
-#         user = super(ClientRegistrationForm, self).save(commit=False)
-#         # Set password randomly generated in the model
-#         user.set_password(user.password1)
-#         if commit:
-#             user.save()
-#         return user
-
-
-# Client = get_user_model()      
-# class ClientForm(forms.ModelForm):
-#     password = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
-#     # password = forms.CharField(label='Confirmer le mot de passe', widget=forms.PasswordInput)
-
-#     class Meta:
-#         model = client
-#         fields = ['last_name', 'first_name', 'email', 'ville', 'adress', 'password']
-
-    # def clean_password2(self):
-    #     # Vérifie que les deux mots de passe correspondent
-    #     password1 = self.cleaned_data.get("password1")
-    #     password2 = self.cleaned_data.get("password2")
-    #     if password1 and password2 and password1 != password2:
-    #         raise forms.ValidationError("Les mots de passe ne correspondent pas")
-    #     return password2
-
-    # def save(self, commit=True):
-    #     # Sauvegarde le mot de passe haché
-    #     client = super(ClientForm, self).save(commit=False)
-    #     client.set_password(self.cleaned_data["password1"])
-    #     if commit:
-    #         client.save()
-    #     return client
